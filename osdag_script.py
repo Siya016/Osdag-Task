f6ee07ecbebf1e71c8b2578c85149abae6233e5c
Siya <148008409+Siya016@users.noreply.github.com>
2025-01-11 17:03:40 +0530

    Update osdag_script.py

--- osdag_script.py
+++ osdag_script.py
@@ -112,7 +112,8 @@ def install_miniconda():
 
         # Install Miniconda silently
         try:
-            subprocess.run([installer_path, "/S", f"/D={os.path.expanduser('~\\\\Miniconda3')}"], check=True)
+            install_path = os.path.expanduser(r"~\Miniconda3")
+            subprocess.run([installer_path, "/S", f"/D={install_path}"], check=True)
             print("Miniconda installed successfully.")
         except subprocess.CalledProcessError as e:
             print(f"Failed to install Miniconda: {e}")
