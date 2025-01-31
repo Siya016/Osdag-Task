a49862306b58073d51e8077d01ae9a714dff0022
Siya <148008409+Siya016@users.noreply.github.com>
2025-01-31 16:58:59 +0530

    Update osdag_script.py

--- osdag_script.py
+++ osdag_script.py
@@ -889,7 +889,7 @@ import shutil
 
 # URLs for Miniconda and MiKTeX
 MINICONDA_URL = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
-MIKTEX_URL = "https://miktex.org/download/installer/x64/basic-miktex-24.1-x64.exe"  # Update to the correct MiKTeX download URL
+MIKTEX_URL = "https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-24.1-x64.exe"  # Update to the correct MiKTeX download URL
 
 # File paths
 MINICONDA_INSTALLER_PATH = os.path.join(os.getcwd(), "MinicondaInstaller.exe")
